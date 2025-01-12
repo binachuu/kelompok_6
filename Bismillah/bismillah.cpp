4a649f177665b2b209b105940d736bef7f34d3ea
unknown <tabinar121004@gmail.com>
2025-01-13 05:44:59 +0700

    bismillah allahuakbar

--- Bismillah/bismillah.cpp
+++ Bismillah/bismillah.cpp
@@ -3,9 +3,8 @@
 */
 #include <iostream>
 #include <GL/glut.h>
-#include <GL/freeglut.h>
 #include <math.h>
-using namespace std;
+
 struct Movement
 {
     float rotate;
@@ -13,8 +12,9 @@ struct Movement
 
 bool isMoving = true;
 bool is2DMode = false;
+bool hidden = false;
 float lightPos[] = {0.0f, 10.0f, 0.0f, 0.0f};
-float aspectRatio = 1.0f; // Menyimpan aspect ratio layar
+float aspectRatio = 1.0f;
 float cameraYPos = 100;
 float reduceYPos = 0.05;
 float rotate = 0.0;
@@ -33,82 +33,142 @@ void display();
 void loadingScreen();
 void kotak(float x, float y, float z, float size);
 void object2D();
-void createMenu();
 void carte();
 void update(int value);
-void processMenu(int option);
 void drawConnectingLines(float time, float x_offset);
 void drawPoint(float x, float y, float intensity);
 void drawText(float x, float y, const std::string &text, void *font, float r, float g, float b);
-/*
---------------------------- Rest Of Function -----------------------------
-*/
+void myKeyboard(unsigned char key, int x, int y);
+
+// Fungsi utama
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(1366, 768);
+    glutInitWindowSize(800, 600);
     glutCreateWindow("Playstation 2");
+
     init3D();
+
     glutDisplayFunc(display);
+    glutKeyboardFunc(myKeyboard); // Daftarkan fungsi keyboard
     glutTimerFunc(1000 / 60, update, 0);
+
     glutMainLoop();
     return 0;
 }
 
+// Fungsi untuk menggambar garis sumbu koordinat (cartesius)
 void carte()
 {
-	glColor3f(1.0, 0.0, 0.0);
-	glBegin(GL_LINES);
-	glVertex3f(-1000.0, 0.0, 0.0);
-	glVertex3f(1000.0, 0.0, 0.0);
-	
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(0.0, -1000.0, 0.0);
-	glVertex3f(0.0, 1000.0, 0.0);
-	
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.0, -1000.0);
-	glVertex3f(0.0, 0.0, 1000.0);
-	glEnd();
+    glColor3f(1.0, 0.0, 0.0); // X-axis (merah)
+    glBegin(GL_LINES);
+    glVertex3f(-1000.0, 0.0, 0.0);
+    glVertex3f(1000.0, 0.0, 0.0);
+
+    glColor3f(0.0, 1.0, 0.0); // Y-axis (hijau)
+    glVertex3f(0.0, -1000.0, 0.0);
+    glVertex3f(0.0, 1000.0, 0.0);
+
+    glColor3f(0.0, 0.0, 1.0); // Z-axis (biru)
+    glVertex3f(0.0, 0.0, -1000.0);
+    glVertex3f(0.0, 0.0, 1000.0);
+    glEnd();
 }
 
-void kotak(float x, float y, float z, float size)
+// Fungsi keyboard untuk mengontrol program
+void myKeyboard(unsigned char key, int x, int y)
 {
-	glPushMatrix();
-	glTranslatef(x, y, z);
-    glRotated(-40.0, 0.0, 1.0, 0.0);
-    glColor3ub(255, 255, 255);
-    glScalef(1.0f, size, 1.0f);
-    glutSolidCube(5.0);
-    glPopMatrix();
-}
-
-void ekor(float startX, float startY, float startZ, float length, int segments)
-{
-    float segmentLength = length / segments;
-    for (int i = 0; i < segments; i++)
+    switch (key)
     {
-        float alpha = 1.0f - (float)i / segments;
-        float offsetX = (rand() % 100 - 50) / 100.0f; // Randomize x-offset
-        float offsetY = (rand() % 100 - 50) / 100.0f; // Randomize y-offset
-        float offsetZ = (rand() % 100 - 50) / 100.0f; // Randomize z-offset
-
-        glColor4f(1.0f, 0.5f + 0.5f * alpha, 0.0f, alpha); // Firework-like color
-        glPushMatrix();
-        glTranslatef(startX + i * segmentLength * 0.8f + offsetX, startY + i * segmentLength * 0.5f + offsetY, startZ - i * segmentLength + offsetZ);
-        glutSolidSphere(1.0f, 10, 10); // Use sphere for a firework particle effect
-        glPopMatrix();
+    case 'c': // Toggle menampilkan/menyembunyikan garis sumbu
+        hidden = !hidden; // Ubah status hidden
+        break;
+    case 27: // Kode ASCII untuk tombol ESC
+        exit(0); // Keluar dari program
+        break;
     }
+    glutPostRedisplay(); // Meminta tampilan untuk di-render ulang
 }
-// Fungsi untuk menggambar teks
+
+// Fungsi utama untuk menggambar tampilan
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(70.0, aspectRatio, 1.0, 1000.0);
+
+    if (cameraYPos <= 90)
+    {
+        reduceYPos += 0.1;
+    }
+    gluLookAt(5.0, cameraYPos -= reduceYPos, 5.0, 0.0, -10.0, 0.0, 0.0, 1.0, 0.0);
+    glMatrixMode(GL_MODELVIEW);
+
+    if (!hidden) // Hanya tampilkan garis sumbu jika tidak disembunyikan
+    {
+        carte();
+    }
+
+    if (!is2DMode && cameraYPos <= 10)
+    {
+        is2DMode = true;
+    }
+    if (is2DMode)
+    {
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluOrtho2D(-5, 5, -5, 5);
+        glMatrixMode(GL_MODELVIEW);
+
+        object2D();
+    }
+    else
+    {
+        glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+        loadingScreen();
+    }
+
+    glutSwapBuffers();
+}
+
+// Fungsi untuk inisialisasi 3D
+void init3D()
+{
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    GLfloat ambientLight[] = {0.3f, 0.3f, 0.3f, 1.0f};
+    GLfloat diffuseLight[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+
+    glClearColor(0.1, 0.1, 0.1, 1.0);
+}
+
+// Fungsi untuk memperbarui rotasi dan posisi
+void update(int value)
+{
+    if (!is2DMode && isMoving)
+    {
+        cube.rotate += 1.2;
+    }
+    glutPostRedisplay();
+    glutTimerFunc(1000 / 60, update, 0);
+}
+
+// Fungsi untuk menggambar loading screen
 void loadingScreen()
 {
     glPushMatrix();
     /*
     -------------------------------- Cube --------------------------------
     */
-	glRotated(rotate -= 0.03, 0, 1, 0);
+    glRotated(rotate -= 0.03, 0, 1, 0);
     kotak(0, 0, 0, 5);
 
     kotak(-10, 0, 5, 10);
@@ -118,9 +178,8 @@ void loadingScreen()
 
     kotak(-20, 0, 10, 10);
     kotak(20, 0, -10, 10);
-    kotak(-15, 0, -15, 10);
     kotak(15, 0, 15, 10);
-    
+
     kotak(-5, -10, 10, 10);
     kotak(5, 15, -15, 10);
     kotak(-10, -15, 5, 10);
@@ -130,21 +189,36 @@ void loadingScreen()
     kotak(25, -15, 10, 10);
     kotak(-25, 5, -10, 10);
     
-    glPushMatrix();
+    kotak(-10, -15, 15, 10);
+    kotak(10, 20, -20, 10);
+    kotak(-15, -20, 10, 10);
+    kotak(20, -15, -25, 10);
+    kotak(10, -10, 25, 10);
+    kotak(-25, 25, -20, 10);
+    kotak(30, -20, 15, 10);
+    kotak(-30, 10, -15, 10);
     
+    kotak(-40, 10, 5, 10);
+    kotak(40, -5, -10, 10);
+    kotak(5, 40, -15, 3);
+    kotak(-5, -40, 10, 10);
+
+    glPushMatrix();
+
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glTranslatef(0.0, 20.0, 0.0);
+    glTranslatef(0.0, 20.0, 0.0);
     glRotated(cube.rotate, 1.0, 1.0, 1.0);
     glColor4ub(128, 128, 128, 128);
     glutSolidCube(10.0);
 
     glDisable(GL_BLEND);
     glPopMatrix();
-    
+
     glPopMatrix();
 }
 
+// Fungsi untuk menggambar objek 2D
 void object2D()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -176,89 +250,16 @@ void object2D()
     glFlush();
 }
 
-void display()
+// Fungsi untuk menggambar kotak
+void kotak(float x, float y, float z, float size)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(70.0, 1.0, 1.0, 1000.0);
-    if (cameraYPos <= 90)
-    {
-    	reduceYPos += 0.1;
-	}
-    gluLookAt(5.0, cameraYPos -= reduceYPos, 5.0, 0.0, -10.0, 0.0, 0.0, 1.0, 0.0);
-    glMatrixMode(GL_MODELVIEW);
-    carte();
-    if (!is2DMode && cameraYPos <= 10) 
-    {
-    	is2DMode = true;
-	}
-    if (is2DMode)
-    {
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluOrtho2D(-5, 5, -5, 5);
-        glMatrixMode(GL_MODELVIEW);
-
-        object2D();
-    }
-    else
-    {
-        glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-        loadingScreen();
-    }
-
-    glutSwapBuffers();
-}
-
-void init3D()
-{
-    /*-------------------------------- 3D Init --------------------------------*/
-
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_COLOR_MATERIAL);
-
-    /*-------------------------------- Pencahayaan --------------------------------*/
-
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    GLfloat ambientLight[] = {0.3f, 0.3f, 0.3f, 1.0f};
-    GLfloat diffuseLight[] = {1.0f, 1.0f, 1.0f, 1.0f};
-    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
-    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-
-    /*-------------------------------- ViewPort --------------------------------*/
-
-    glClearColor(0.1, 0.1, 0.1, 1.0);
-}
-
-void processMenu(int option)
-{
-    switch (option)
-    {
-    case 1:
-        is2DMode = false;
-        init3D();
-        break;
-    case 2:
-        is2DMode = true;
-        break;
-    }
-    glutPostRedisplay();
-}
-
-void update(int value)
-{
-    if (!is2DMode)
-    {
-        if (isMoving)
-        {
-            cube.rotate += 1.2;
-        }
-        glutPostRedisplay();
-    }
-    glutTimerFunc(1000 / 60, update, 0);
+    glPushMatrix();
+    glTranslatef(x, y, z);
+    glRotated(-40.0, 0.0, 1.0, 0.0);
+    glColor3ub(255, 255, 255);
+    glScalef(1.0f, size, 1.0f);
+    glutSolidCube(5.0);
+    glPopMatrix();
 }
 
 void drawPoint(float x, float y, float intensity)
